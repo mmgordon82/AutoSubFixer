52c59cfcd9b9f1c6f634b37953a64884454ec2bf
Maor Gordon <mmgordon82@gmail.com>
2022-07-06 19:37:19 +0300

    added cross-platform building

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal
 
 set VERSION=0.0.1a
 set OUTPUT=dist
@@ -17,19 +18,20 @@ echo [+] Building version %VERSION% to /%OUTPUT%...
 echo.
 
 :: If there are parameters, add them to go build command
-IF ["%LDFLAGS_PARAMETERS%"]==[""] (
+if ["%LDFLAGS_PARAMETERS%"]==[""] (
     set LDFLAGS_BUILD=
-) ELSE (
+) else (
     set LDFLAGS_BUILD=-ldflags %LDFLAGS_PARAMETERS%
 )
+
+:: Remove Previous Build
+del %OUTPUT% /q >nul
 
 
 :: Create Syso files
 echo [+] Building syso files...
-go-winres make --in installer\winres\winres.json --product-version=%VERSION% --file-version=%VERSION%
+"%GOPATH%\bin\go-winres.exe" make --in installer\winres\winres.json --product-version=%VERSION% --file-version=%VERSION%
 
-:: Remove Previous Build
-del dist /q
 
 :: Create the main executable file
 go build -o %OUTPUT%/AutoSubFixer.exe %LDFLAGS_BUILD%
@@ -39,10 +41,35 @@ echo [+] Creating setup...
 "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" "/DMyAppVersion=%VERSION%" "/DMyDistFolder=%OUTPUT%" installer\install.iss 1>nul
 
 :: Rename to Portable
-move %OUTPUT%\AutoSubFixer.exe %OUTPUT%\AutoSubFixer-Portable.exe
+move %OUTPUT%\AutoSubFixer.exe %OUTPUT%\AutoSubFixer-Portable.exe >nul
+
+
+:: Build for Other OSes
+set GOOS=darwin
+call :Build_OS_ARCH amd64 macOS
+call :Build_OS_ARCH arm64 macOS
+
+set GOOS=linux
+call :Build_OS_ARCH amd64 linux
+call :Build_OS_ARCH 386 linux
+
 
 :: Delete residues
 echo [+] Cleaning up...
 del *.syso
 
-echo Done!
+
+echo Done! Press any key to exit...
+endlocal
+pause >nul
+goto :eof
+
+:: Usage:
+::      set GOOS=<OS-TYPE>
+::      call Build_OS_ARCH <GOARCH> <OS-NAME>
+:Build_OS_ARCH
+    set GOARCH=%1
+    echo [+] Building for %2-%GOARCH%...
+    go build -o %OUTPUT%/autosubfixer
+    powershell -Command "Compress-Archive %OUTPUT%/autosubfixer dist\AutoSubFixer-%2-%GOARCH%.zip"
+    del %OUTPUT%\autosubfixer
--- go.mod
+++ go.mod
@@ -10,5 +10,5 @@ require (
 require (
 	github.com/sirupsen/logrus v1.7.0 // indirect
 	golang.org/x/net v0.0.0-20200930145003-4acb6c075d10 // indirect
-	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
+	golang.org/x/sys v0.0.0-20220704084225-05e143d24a9e // indirect
 )
